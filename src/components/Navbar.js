a2f47e7460367fa358a7d89297854cea1ebfbd0f
msahmed646 <msahmed646@gmail.com>
2023-07-18 10:59:51 -0400

    Responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,39 @@
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import "./NavbarStyles.css"
+import "./NavbarStyles.css";
 
-import React from 'react'
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => {
+        setClick(!click);
+    }
   return (
     <div className="header">
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
 
-        <ul className="nav-menu">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/project">Project</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
+        <ul className={click? "nav-menu active": "nav-menu"}>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/project">Project</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
         </ul>
+        
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (<FaTimes size={20} style={{ color: 'whitesmoke' }} />) : (<FaBars size={20} style={{ color: 'whitesmoke' }} />)};
+        </div>
     </div>
-  )
+    )
 }
 export default Navbar;
