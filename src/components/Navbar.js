97f925c1a63e520c24769bd0280b940c677f8fb1
msahmed646 <msahmed646@gmail.com>
2023-07-18 03:21:18 -0400

    Add navbar and button styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,21 @@ const Navbar = () => {
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
+
+        <ul className="nav-menu">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/project">Project</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
     </div>
   )
 }
