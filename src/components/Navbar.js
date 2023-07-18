821c2851a94e46e0d4fe1286ab2e46dafdb53c67
msahmed646 <msahmed646@gmail.com>
2023-07-18 14:05:42 -0400

    Navbar-fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,31 +9,43 @@ const Navbar = () => {
     const handleClick = () => {
         setClick(!click);
     }
-  return (
-    <div className="header">
-        <Link to="/">
-            <h1>Portfolio</h1>
-        </Link>
 
-        <ul className={click? "nav-menu active": "nav-menu"}>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/project">Project</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contact</Link>
-            </li>
-        </ul>
-        
-        <div className="hamburger" onClick={handleClick}>
-            {click ? (<FaTimes size={20} style={{ color: 'whitesmoke' }} />) : (<FaBars size={20} style={{ color: 'whitesmoke' }} />)};
+    const [color, setColor] = useState(false);
+    const changeColor = () => {
+        if(window.scrollY >= 100){
+            setColor(true);
+        }else{
+            setColor(false);
+        }
+    };
+
+    window.addEventListener("scroll", changeColor);
+
+    return (
+        <div className={color ? "header header-bg" : "header"}>
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/project">Project</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+            
+            <div className="hamburger" onClick={handleClick}>
+                {click ? (<FaTimes size={20} style={{ color: 'whitesmoke' }} />) : (<FaBars size={20} style={{ color: 'whitesmoke' }} />)};
+            </div>
         </div>
-    </div>
     )
 }
 export default Navbar;
